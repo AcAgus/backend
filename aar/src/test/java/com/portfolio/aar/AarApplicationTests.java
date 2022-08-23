acf785f22cbb6ed6c4c8fa91772ca93f54ab9dd0
AcAgus <aaguss16@gmail.com>
2022-08-23 15:38:17 -0300

    creacion repo

---
+++ aar/src/test/java/com/portfolio/aar/AarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
